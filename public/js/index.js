9a9cd61590aa89468f80401e94b2f70782b1f0d1
Hồ Đức Nguyên <hdnguyen7702@gmail.com>
2023-11-25 19:14:18 +0700

    Thêm room chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,98 @@
 
 
+const socket = io('http://localhost:3000') 
+
+// kiểm tra xem đẫ kết nối chưa 
+let isLogin = true
+
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+const containerLogin = $("#container-login")
+const containerChat = $("#container-chat")
+const formlogin = $("#form-login")
+const inputUsername = $("input[name='username']")
+const btnNewChatRoom = $("#btn-new-chat-room") 
 
 
 
-const userInfo = document.getElementById('user-info')
-let socket 
 
-userInfo.addEventListener('submit', event => { 
-    socket = io('http://localhost:3000') // thực hiện kết nối. 
+
+changeScreen()
+
+
+
+
+
+// tiếp theo là bắt đầu sự kiện send đi nè. 
+// lấy ra sự kiện nó submit 
+
+// lưu lại biến username sau khi submit đi. 
+
+
+
+
+// * EVENT 
+formlogin.addEventListener('submit',function(event) { 
+    event.preventDefault();
+    // ! chưa check
+    emitSendUserName({username: inputUsername.value})
+    isLogin = false     
+    changeScreen()
+}) 
+btnNewChatRoom.addEventListener('click', function(event) { 
+    let nameRoom= $("input[name='name-chat-room']").value 
+    let passwordRoom = $("input[name='password-chat-room']").value 
+    emitNewChatRoom(nameRoom, passwordRoom, inputUsername.value)
 })
+
+
+
+
+// * EMIT 
+let emitSendUserName = function(data) { 
+    socket.emit("sendUsername", data)
+}
+
+let emitNewChatRoom = function(data) { 
+    socket.emit("newChatRoom", data)
+}
+
+
+// * ON 
+socket.on('sendListRoom', roomsDto => { 
+    const ulRooms = $("#ul-rooms") 
+    // tiếp theo là chuẩn bị render 
+    let iconScope
+    // nhận ở đây là object không phải là là array. 
+    let listRoomHtml = ""
+    for (const nameRoom in roomsDto) {
+        if (roomsDto[nameRoom].isPassword == true) {    
+            iconScope = '<i class="fa-solid fa-lock"></i>'
+        }
+        else { 
+            iconScope = '<i class="fa-solid fa-globe"></i>'
+        }
+        listRoomHtml +=  `
+            <li class="clearfix">
+                <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="avatar" />
+                <div class="about">
+                    <div class="name">${nameRoom}</div>
+                    <div class="status">${iconScope}</div>
+                </div>
+         </li>`
+    }
+    ulRooms.innerHTML = listRoomHtml
+})
+
+
+// * FUNC
+function changeScreen() { 
+    if (isLogin) { 
+        containerChat.style.display = "none"
+        containerLogin.style.display = "block"
+    }
+    else { 
+        containerChat.style.display = "block"
+        containerLogin.style.display = "none"
+    }
+}
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const path = require('path')
 const hbs = require('hbs')
 const bodyParser = require('body-parser')
 const mssql = require('mssql')  
+const { sign } = require('crypto')
 
 
 const config = {
@@ -20,61 +21,113 @@ const config = {
 
 
 // TEST kết nối cơ sở dữ liệu
-mssql.connect(config, err => {
-    if (err) { 
-        console.log(err)
-    }
-    let request = new mssql.Request() 
-    request.query('select * from activitys', function (err, wrapper) {
+// mssql.connect(config, err => {
+//     if (err) { 
+//         console.log(err)
+//     }
+//     let request = new mssql.Request() 
+//     request.query('select * from activitys', function (err, wrapper) {
             
-        if (err) console.log(err)
-        console.log(wrapper.recordset)
-    });
-})
+//         if (err) console.log(err)
+//         console.log(wrapper.recordset)
+//     });
+// })
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false })  
 
 const app = express()  
 const server = http.createServer(app)
 const io = socketIO(server)
+hbs.registerPartials('D:/Chat RPC/views/partials')
 app.set('view engine', 'hbs')
 app.use(express.static( 'public'))
-hbs.registerPartials(path.join(__dirname, 'views/layouts'))
 
 
-const rooms = {}
+// nên lưu trữ thành object thay vì thành data 
 
-let isAddUser = false 
+
+
+const rooms = {} // Lưu trữ room trên hệ thống. 
+const roomsDto = {} // không chứa passWord trong hệ thống. 
+const users = []
+
+// check user exist  
+
+function isUserExist(userCheck) { 
+    users.forEach(user => { 
+        if (userCheck.username == user.username) {
+            return true
+        }
+    })
+    return false 
+}
+
+function isRoomExist(roomCheck) { 
+    rooms.forEach(room => { 
+        if (room.name == roomCheck.name) {
+            return true
+        }
+    })
+    return false 
+}
+
 
 
 // on lắng nghe cái gì đó  
 io.on('connection', socket => { // sự kiến có người dùng kết nối
-    
-    if (!isAddUser) return 
 
+    console.log(`Người dùng đã join ${socket.id}`)
     
+    socket.on('sendUsername', data => { 
+        let user = {idSocket: socket.id, username: data.username}
+        if (isUserExist(user)) return 
+        users.push(user)    
+        socket.emit('sendListRoom',roomsDto)
+        
+    }) 
 
-    socket.on('client_event', (data) => {
-        console.log('Dữ liệu từ máy khách:', data)
-    });
-    socket.emit('server_event', { message: 'Chào mừng đến với máy chủ!' })
+    socket.on('joinRoom', (nameRoom, passwordRoom, username) => { 
+        let room = rooms[nameRoom]
+        if (room.password != passwordRoom) { 
+            // không thể join vào room này
+            console.log("Sai mật khẩu rồi")
+            return 
+        }
+        // cho người dùng join vào room
+        room.users.add(username) 
+        socket.join(nameRoom)
+    })
+
+
+
+    socket.on('newChatRoom',(nameRoom, passwordRoom, username) => { // data: name: password
+        let isPassword = false ? passwordRoom == '' : true
+        
+        rooms[nameRoom] = { 
+            password: passwordRoom,
+            users: new Set(username)
+        }
+
+        roomsDto[nameRoom] = { 
+            isPassword, 
+            users: new Set(username)
+        }
+        // cho socket join vào cái room đó. 
+        socket.join(nameRoom)
+        io.emit('sendListRoom',roomsDto) // * Thông báo đến tất cả người dùng là có room mới
+    })
 })
+
 
 
 
 app.get('/', (req, res) => {
-    res.render('join', {layout: 'layouts/main'}) 
+    res.render('client') 
 }) 
 
 
-app.post('/join',urlencodedParser,(req, res) => {
-    console.log(req.body.username)
-    res.render('join', {layout: 'layouts/main'})
-})
 
-app.get('/chat', ((req, res) => {
-    res.render('chat', {layout: 'layouts/main'})
-}))  
+
 
 const PORT = 3000
 // ipv4: 10.251.8.76 
